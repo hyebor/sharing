89a7beabc74007395d5d635b53ae14dce5379010
hyebor <1711347203@qq.com>
2021-01-26 00:35:18 +0800

    '完成了权限功能的开发'

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main,
         Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card,
-        Col, Row, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox } from 'element-ui'
+        Col, Row, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox,
+        Tag, Tree, Select, Option
+} from 'element-ui'
 
 Vue.use(Button);
 Vue.use(Form);
@@ -26,6 +28,10 @@ Vue.use(Switch);
 Vue.use(Tooltip);
 Vue.use(Pagination);
 Vue.use(Dialog);
+Vue.use(Tag);
+Vue.use(Tree);
+Vue.use(Select);
+Vue.use(Option);
 
 Vue.prototype.$message = Message;
 Vue.prototype.$confirm = MessageBox.confirm;
